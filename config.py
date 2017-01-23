2139348cae595419fa497bac2ff509cf0f57c5de
toert <pw177@mail.ru>
2017-01-23 23:20:14 +0300

    first initial

---
+++ config.py
@@ -0,0 +1,3 @@
+#DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://score:Rysherat2@shopscore.devman.org:5432/shop'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
--- server.py
+++ server.py
@@ -1,10 +1,42 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from datetime import datetime, timedelta, date
 
 app = Flask(__name__)
 
+app.config.from_object('config')
+db = SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
+
+
+class Orders(db.Model):
+    __tablename__ = 'orders'
+
+
 @app.route('/')
 def score():
-    return render_template('score.html')
+    undone_orders_query = Orders.query.filter(Orders.confirmed.is_(None)).order_by(Orders.created)
+    done_today = len(Orders.query.filter(func.date(Orders.confirmed) == date.today()).all())
+    score = datetime.today() - undone_orders_query.first().created
+    score_in_string = '{}:{}:{}'.format(score.days * 24 + score.seconds // 3600,  # hours
+                                        (score.seconds % 3600) // 60,  # minutes
+                                        score.seconds % 60)  # seconds
+    if score > timedelta(minutes=30):
+        color = 'firebrick'
+    elif score > timedelta(minutes=7):
+        color = 'darkgoldenrod'
+    else:
+        color = 'darkgreen'
+    return render_template('score.html',
+                           now=datetime.today(),
+                           seven_minutes=timedelta(minutes=7),
+                           thirty_minutes=timedelta(minutes=30),
+                           orders=undone_orders_query.all(),
+                           done_today=done_today,
+                           score=score_in_string,
+                           color=color)
+
 
 if __name__ == "__main__":
     app.run()
